380717f4d72f451fb2b7d4ee9ff4214647f160d8
HARISARAVANAN <115549443+hari3112@users.noreply.github.com>
2025-02-12 20:06:30 +0530

    Add files via upload

---
+++ controlbot_bringup/launch/real_robot.launch.py
@@ -0,0 +1,39 @@
+import os
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+
+
+def generate_launch_description():
+    hardware_interface = IncludeLaunchDescription(
+        os.path.join(
+            get_package_share_directory("controlbot_firmware"),
+            "launch",
+            "hardware_interface.launch.py"
+        ),
+    )
+    
+    controller = IncludeLaunchDescription(
+        os.path.join(
+            get_package_share_directory("controlbot_controllers"),
+            "launch",
+            "controller.launch.py"
+        ),
+        launch_arguments={
+            "use_simple_controller": "False",
+            "use_python": "False"
+        }.items()
+    )
+    
+
+    imu_driver_node = Node(
+        package="controlbot_firmware",
+        executable="mpu6050_driver.py"
+    )
+    
+    return LaunchDescription([
+        hardware_interface,
+        controller,
+        imu_driver_node,
+    ])
---
+++ controlbot_bringup/launch/simulated_robot.launch.py
@@ -0,0 +1,34 @@
+import os
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from ament_index_python.packages import get_package_share_directory
+
+
+def generate_launch_description():
+    gazebo = IncludeLaunchDescription(
+        os.path.join(
+            get_package_share_directory("controlbot_1_imu_description"),
+            "launch",
+            "gazebo.launch.py"
+        ),
+    )
+    
+    controller = IncludeLaunchDescription(
+        os.path.join(
+            get_package_share_directory("controlbot_controllers"),
+            "launch",
+            "controller.launch.py"
+        ),
+        launch_arguments={
+            "use_simple_controller": "False",
+            "use_python": "True"
+        }.items(),
+    )
+    
+  
+    
+    return LaunchDescription([
+        gazebo,
+        controller,
+       
+    ])
