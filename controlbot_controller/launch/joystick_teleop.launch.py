2fcfc89c1cd65f0aad521c7e947850227c1e34ed
HARISARAVANAN <115549443+hari3112@users.noreply.github.com>
2025-03-25 13:30:17 +0530

    Add files via upload

--- controlbot_controller/launch/joystick_teleop.launch.py
+++ controlbot_controller/launch/joystick_teleop.launch.py
@@ -1,7 +1,7 @@
 import os
 
 from ament_index_python.packages import get_package_share_directory
-
+from launch.actions import IncludeLaunchDescription
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration
@@ -9,6 +9,8 @@ from launch_ros.actions import Node
 
 
 def generate_launch_description():
+
+    controlbot_controller_pkg = get_package_share_directory('controlbot_controller')
 
     use_sim_time_arg = DeclareLaunchArgument(name="use_sim_time", default_value="True",
                                       description="Use simulated time"
@@ -29,10 +31,26 @@ def generate_launch_description():
                     {"use_sim_time": LaunchConfiguration("use_sim_time")}]
     )
 
+    twist_mux_launch = IncludeLaunchDescription(
+        os.path.join(
+            get_package_share_directory("twist_mux"),
+            "launch",
+            "twist_mux_launch.py"
+        ),
+        launch_arguments={
+            "cmd_vel_out": "controlbot_controller/cmd_vel_unstamped",
+            "config_locks": os.path.join(controlbot_controller_pkg, "config", "twist_mux_locks.yaml"),
+            "use_sim_time": LaunchConfiguration("use_sim_time"),
+        }.items(),
+    )
+
+    
     return LaunchDescription(
         [
             use_sim_time_arg,
             joy_teleop,
-            joy_node
+            joy_node,
+            twist_mux_launch,
+            
         ]
     )
