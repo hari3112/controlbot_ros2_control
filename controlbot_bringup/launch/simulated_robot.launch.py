aa4261278e6fad80c82a7cc82aca5bb5288fbf2b
HARISARAVANAN <115549443+hari3112@users.noreply.github.com>
2025-04-02 13:34:52 +0530

    Add files via upload

--- controlbot_bringup/launch/simulated_robot.launch.py
+++ controlbot_bringup/launch/simulated_robot.launch.py
@@ -1,10 +1,20 @@
 import os
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
+from launch.conditions import IfCondition, UnlessCondition
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 
 
 def generate_launch_description():
+    use_slam = LaunchConfiguration("use_slam")
+
+    use_slam_arg = DeclareLaunchArgument(
+        "use_slam",
+        default_value="false"
+    )
+
     gazebo = IncludeLaunchDescription(
         os.path.join(
             get_package_share_directory("controlbot_1_imu_description"),
@@ -25,19 +35,60 @@ def generate_launch_description():
         }.items(),
     )
     
-    joystick = IncludeLaunchDescription(
+
+
+    localization = IncludeLaunchDescription(
         os.path.join(
-            get_package_share_directory("controlbot_controller"),
+            get_package_share_directory("controlbot_localization"),
             "launch",
-            "joystick_teleop.launch.py"
+            "global_localization.launch.py"
         ),
-        launch_arguments={
-            "use_sim_time": "True"
-        }.items()
+        condition=UnlessCondition(use_slam)
+    )
+
+    slam = IncludeLaunchDescription(
+        os.path.join(
+            get_package_share_directory("controlbot_mapping"),
+            "launch",
+            "slam.launch.py"
+        ),
+        condition=IfCondition(use_slam)
+    )
+
+    rviz_localization = Node(
+        package="rviz2",
+        executable="rviz2",
+        arguments=["-d", os.path.join(
+                get_package_share_directory("controlbot_localization"),
+                "rviz",
+                "global_localization.rviz"
+            )
+        ],
+        output="screen",
+        parameters=[{"use_sim_time": True}],
+        condition=UnlessCondition(use_slam)
+    )
+
+    rviz_slam = Node(
+        package="rviz2",
+        executable="rviz2",
+        arguments=["-d", os.path.join(
+                get_package_share_directory("controlbot_mapping"),
+                "rviz",
+                "slam.rviz"
+            )
+        ],
+        output="screen",
+        parameters=[{"use_sim_time": True}],
+        condition=IfCondition(use_slam)
     )
     
     return LaunchDescription([
+        use_slam_arg,
         gazebo,
         controller,
-        joystick,
+        localization,
+        slam,
+        rviz_localization,
+        rviz_slam
     ])
